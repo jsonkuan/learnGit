8a76ae256cad30354aff1b64d64acd93734539f7
Jason Kuan <jsonkuan@gmail.com>
2016-10-11 10:26:56 +0200

    Revert "Implemented malware"
    This reverts commit 65b58ec4995ddf34c6c60fa6b84209ff5024df09.

--- code.java
+++ code.java
@@ -2,7 +2,4 @@ public class Code {
 	public static void main(String [] args){
 		System.out.print("This is a complex algorithm");
 	}
-	public void maliciousSoftware(){
-		//TODO: Implement some bad ass shit	
-	}
 }
