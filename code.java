65b58ec4995ddf34c6c60fa6b84209ff5024df09
Jason Kuan <jsonkuan@gmail.com>
2016-10-11 10:24:39 +0200

    Implemented malware

--- code.java
+++ code.java
@@ -2,4 +2,7 @@ public class Code {
 	public static void main(String [] args){
 		System.out.print("This is a complex algorithm");
 	}
+	public void maliciousSoftware(){
+		//TODO: Implement some bad ass shit	
+	}
 }
