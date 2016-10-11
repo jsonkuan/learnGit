089a900000d3291b777b5c7ec62f4b05907ad884
Gualberto Scolari <gualbertoscolari@Apples-MacBook-Pro-4.local>
2016-10-11 11:46:07 +0200

    ew method again

--- code.java
+++ code.java
@@ -2,7 +2,14 @@ public class Code {
 	public static void main(String [] args){
 		System.out.print("This is a complex algorithm");
 	}
+	
 	public void hello(){
-	System.out.println("asfasfaf";)
+		System.out.println("asfasfaf";)
 	}
+	
+	public void destroyComputer() {
+		computerLife = 0;
+
+	}
+	
 }
