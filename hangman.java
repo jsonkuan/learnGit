93f0c5678eb7388a6e54a3ce85bdb940078733b8
guasc0 <gual.scolari@hotmail.com>
2016-10-28 17:55:54 +0200

    added Todo for important epic method

--- hangman.java
+++ hangman.java
@@ -1,5 +1,10 @@
-//TODO: MAKE HANGMAN
+//TODO: MAKE HANGMAN 
+	public checkGuess() { 
+		//TODO: Write a function that checks is guess is correct
+	}
 
-public checkGuess() {
-	//TODO: Write a function that checks is guess is correct	
-    }
+
+	public compareResult() {
+//TODO: Fix method that compares results.
+
+}
