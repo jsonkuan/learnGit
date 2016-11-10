49003e8494c303762c1cc6b4f833aa552955a1e6
Wilhelm Fors <askims_ik94w@hotmail.com>
2016-11-10 15:17:17 +0100

    adding /learnGit/

---
+++ learnGit/CONTRIBUTING.md
@@ -0,0 +1,7 @@
+#Contributors:
+
+###### ```jas0n-```
+#####```Panda :panda_face:``` 
+
+Add new feature below: 
+
---
+++ learnGit/code.java
@@ -0,0 +1,33 @@
+public class Code {
+	public static void main(String [] args){
+		System.out.print("This is a complex algorithm");
+	}
+	
+	public void hello(){
+		System.out.println("Hello, World";)
+	}
+	
+	public void destroyComputer() {
+		computerLife = 0;
+
+	}
+	
+	public static void int getCompterLife() {
+		return computerLife();
+	}
+	
+	public String hangman() {
+		return completeHangmanGame;
+	}
+
+	private String soImportant(String hej) {
+		return hej;
+	}
+
+	// Undrar om det går att pusha det här?
+
+	//Hej på er, ni behöver den här koden!
+
+
+	
+}
