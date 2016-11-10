d7f3715642832532b1a87b70b755b04bb92509a6
ASA <asa.kwarnmark@gmail.com>
2016-11-10 14:50:26 +0100

    Testing!

--- Training/app/src/main/java/com/cjcmalm/konsumentapp/MainActivity.java
+++ Training/app/src/main/java/com/cjcmalm/konsumentapp/MainActivity.java
@@ -10,4 +10,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public static void wooHoo() {
+        int testing = 4;
+    }
 }
